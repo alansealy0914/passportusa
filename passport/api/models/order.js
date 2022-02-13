93ec113e8cca5fa27b5661314e874c6d83cee843
alansealy0914 <alan.sealy@gmail.com>
2022-02-12 22:02:21 -0600

    Update order.js

--- passport/api/models/order.js
+++ passport/api/models/order.js
@@ -1,1 +1,60 @@
 
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  custname: {
+    type: String,
+    required: true
+  },
+  Address: {
+    type: String,
+    required: true
+  },
+  busnumber: {
+    type: String,
+    required: true
+  },
+  ordernumber: {
+      type: String,
+      required: true
+  },
+  partnumber: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  items: {
+    type: String,
+    count: Number,
+    required: true
+  },
+  orderdate: {
+    type: String,
+    required: false
+  }
+});
+
+mongoose.model('Order', orderSchema);
+
+
+/*
+const Schema = mongoose.Schema;
+
+let Book = new Schema({
+  name: {
+    type: String
+  },
+  price: {
+    type: String
+  },
+  description: {
+    type: String
+  }
+}, {
+  collection: 'books'
+})
+
+module.exports = mongoose.model('Book', Book)*/
