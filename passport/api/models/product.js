7f0f3dc9f74ed2dc05c2a35770f8fd7afec3ab17
alansealy0914 <alan.sealy@gmail.com>
2022-02-12 22:03:00 -0600

    Update product.js

--- passport/api/models/product.js
+++ passport/api/models/product.js
@@ -1,1 +1,21 @@
 
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+ 
+  partnumber: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+
+});
+
+mongoose.model('Product', productSchema);
