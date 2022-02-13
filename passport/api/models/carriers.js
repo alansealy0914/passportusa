c064ef9688e3ac4db80defe0413aa8153c664722
alansealy0914 <alan.sealy@gmail.com>
2022-02-12 22:00:11 -0600

    Update carriers.js

--- passport/api/models/carriers.js
+++ passport/api/models/carriers.js
@@ -1,1 +1,26 @@
 
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+let Carriers = new Schema({
+  carriername: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  
+  trucknumber: {
+    type: String
+  },
+
+  status: {
+    type: Boolean
+  }
+}, {
+  collection: 'carriers'
+})
+
+module.exports = mongoose.model('Carriers', Carriers)
