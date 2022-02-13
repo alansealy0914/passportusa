6d62e8d6e5dadc6eae314f57f31ea0a5e763c049
alansealy0914 <alan.sealy@gmail.com>
2022-02-12 22:01:00 -0600

    Update customer.js

--- passport/api/models/customer.js
+++ passport/api/models/customer.js
@@ -1,1 +1,35 @@
 
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+  custname: {
+    type: String,
+    required: true
+  },
+  busnumber: {
+    type: String,
+    required: true
+  },
+  phone: {
+      type: String,
+      required: true
+  },
+  city: {
+    type: String,
+    required: false
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  zipcode: {
+    type: String,
+    required: false
+  },
+  country: {
+      type: String,
+      required: true
+  }
+});
+
+mongoose.model('Customer', customerSchema);
