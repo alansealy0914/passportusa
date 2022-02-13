c71da0b0d0d78069dc7af357a0aa1137470dbf6e
alansealy0914 <alan.sealy@gmail.com>
2022-02-13 14:57:37 -0600

    Add files via upload

---
+++ passport/client/src/app/products/products.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  title = 'Product Parts';
+  constructor(private router: Router) { }
+
+  goToCreateOrder() {
+    this.router.navigate(['/orders/create']);
+  }
+  ngOnInit(): void {
+  }
+
+}
