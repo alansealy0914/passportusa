cac7e8cf26bdde4709b790d92add606eacd761e3
alansealy0914 <alan.sealy@gmail.com>
2022-02-13 15:04:15 -0600

    Update crud.service.ts

--- passport/client/src/app/service/crud.service.ts
+++ passport/client/src/app/service/crud.service.ts
@@ -1,1 +1,158 @@
 
+/*import { Injectable } from '@angular/core';
+import { Book } from './Book';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+ 
+export class CrudService {
+ 
+  // Node/Express API
+  REST_API: string = 'http://localhost:8000/api';
+ 
+  // Http Header
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+ 
+  constructor(private httpClient: HttpClient) { }
+ 
+  // Add
+  AddCarrier(data: Carrier): Observable<any> {
+    let API_URL = `${this.REST_API}/add-carrier`;
+    return this.httpClient.post(API_URL, data)
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+ 
+  // Get all objects
+  GetCarriers() {
+    return this.httpClient.get(`${this.REST_API}`);
+  }
+ 
+  // Get single object
+  GetCarrier(id:any): Observable<any> {
+    let API_URL = `${this.REST_API}/read-carrier/${id}`;
+    return this.httpClient.get(API_URL, { headers: this.httpHeaders })
+      .pipe(map((res: any) => {
+          return res || {}
+        }),
+        catchError(this.handleError)
+      )
+  }
+ 
+  // Update
+  updateCarrier(id:any, data:any): Observable<any> {
+    let API_URL = `${this.REST_API}/update-carrier/${id}`;
+    return this.httpClient.put(API_URL, data, { headers: this.httpHeaders })
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+ 
+  // Delete
+  deleteCarrier(id:any): Observable<any> {
+    let API_URL = `${this.REST_API}/delete-carrier/${id}`;
+    return this.httpClient.delete(API_URL, { headers: this.httpHeaders}).pipe(
+        catchError(this.handleError)
+      )
+  }
+ 
+ 
+  // Error 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Handle client error
+      errorMessage = error.error.message;
+    } else {
+      // Handle server error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+ 
+*/
+
+import { Injectable } from '@angular/core';
+import { Carriers } from './Carriers';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+ 
+export class CrudService {
+ 
+  // Node/Express API
+  REST_API: string = 'http://localhost:8000/api';
+ 
+  // Http Header
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+ 
+  constructor(private httpClient: HttpClient) { }
+ 
+  // Add
+  AddCarrier(data: Carriers): Observable<any> {
+    let API_URL = `${this.REST_API}/add-carrier`;
+    return this.httpClient.post(API_URL, data)
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+ 
+  // Get all objects
+  GetCarriers() {
+    return this.httpClient.get(`${this.REST_API}`);
+  }
+ 
+  // Get single object
+  GetCarrier(id:any): Observable<any> {
+    let API_URL = `${this.REST_API}/read-carrier/${id}`;
+    return this.httpClient.get(API_URL, { headers: this.httpHeaders })
+      .pipe(map((res: any) => {
+          return res || {}
+        }),
+        catchError(this.handleError)
+      )
+  }
+ 
+  // Update
+  updateCarrier(id:any, data:any): Observable<any> {
+    let API_URL = `${this.REST_API}/update-carrier/${id}`;
+    return this.httpClient.put(API_URL, data, { headers: this.httpHeaders })
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+ 
+  // Delete
+  deleteCarrier(id:any): Observable<any> {
+    let API_URL = `${this.REST_API}/delete-carrier/${id}`;
+    return this.httpClient.delete(API_URL, { headers: this.httpHeaders}).pipe(
+        catchError(this.handleError)
+      )
+  }
+ 
+ 
+  // Error 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Handle client error
+      errorMessage = error.error.message;
+    } else {
+      // Handle server error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+ 
+}
