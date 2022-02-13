3973918c72b638edcd160f2ba1a3c4750979810d
alansealy0914 <alan.sealy@gmail.com>
2022-02-12 22:01:45 -0600

    Update db.js

--- passport/api/models/db.js
+++ passport/api/models/db.js
@@ -1,1 +1,22 @@
 
+require('./users');
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://localhost:27017/mean';
+//const dbURI = 'mongodb://localhost:27017/aposDB';
+
+mongoose.set('useCreateIndex', true);
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose connection error: ${err}`);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
